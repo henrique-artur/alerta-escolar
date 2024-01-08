de6ed410d3f1ecf6687cdbba909b511589e70e4e
artur henrique <henrique-artur@hotmail.com>
2024-01-08 16:41:15 -0300

    Add filter modal in alert list page

--- src/ui/web/pages/admin/alert/list/index.tsx
+++ src/ui/web/pages/admin/alert/list/index.tsx
@@ -1,17 +1,32 @@
 import AlertList from "@web/components/AlertList";
+import ModalFilter from "@web/components/ModalFilter";
+import { useModalFilter } from "@web/components/ModalFilter/hooks";
 import View from "@web/components/base/View";
+import { Button } from "antd";
+import { FaFilter } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 function AlertListPage() {
 	const navigate = useNavigate();
+	const modalFilterRef = useModalFilter();
 
 	return (
-		<View>
+		<View
+			rightButton={
+				<Button
+					type="default"
+					onClick={() => modalFilterRef.current.open()}
+				>
+					<FaFilter size={24} />
+				</Button>
+			}
+		>
 			<AlertList
 				detailsOnClick={(id) =>
 					navigate(`/admin/detalhes-alerta/${id}`)
 				}
 			/>
+			<ModalFilter ref={modalFilterRef} />
 		</View>
 	);
 }
--- src/ui/web/pages/agent/alert/list/index.tsx
+++ src/ui/web/pages/agent/alert/list/index.tsx
@@ -1,17 +1,32 @@
 import View from "@web/components/base/View";
 import { useNavigate } from "react-router-dom";
 import AlertList from "@web/components/AlertList";
+import { Button } from "antd";
+import { FaFilter } from "react-icons/fa";
+import { useModalFilter } from "@web/components/ModalFilter/hooks";
+import ModalFilter from "@web/components/ModalFilter";
 
 function AlertListPage() {
 	const navigate = useNavigate();
+	const modalFilterRef = useModalFilter();
 
 	return (
-		<View>
+		<View
+			rightButton={
+				<Button
+					type="default"
+					onClick={() => modalFilterRef.current.open()}
+				>
+					<FaFilter size={24} />
+				</Button>
+			}
+		>
 			<AlertList
 				detailsOnClick={(id) =>
 					navigate(`/agente/detalhes-alerta/${id}`)
 				}
 			/>
+			<ModalFilter ref={modalFilterRef} />
 		</View>
 	);
 }
