9f42f60028c82f93770f661dda94f8230d350af6
J3Mateus <jmrc1@aluno.ifal.edu.br>
2024-01-09 20:50:01 -0300

    fix:add suporte PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,47 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import EnvironmentPlugin from "vite-plugin-environment";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+	registerType: "prompt",
+	includeAssets: ["favicon.ico", "apple-icon-180x180.png", "masked-icon.svg"],
+	manifest: {
+		name: "Alerta Escolar",
+		short_name: "Alerta Escolar",
+		description: "Aplicativo de monitoramento de alertas escolares oferece uma solução eficiente e prática para manter pais, alunos e educadores.",
+		icons: [
+			{
+				src: "/android-icon-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/android-icon-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+			{
+				src: "/apple-icon-180x180.png",
+				sizes: "180x180",
+				type: "image/png",
+				purpose: "apple touch icon",
+			},
+			{
+				src: "/windows11-icon-225x225",
+				sizes: "225x225",
+				type: "image/png",
+				purpose: "any maskable",
+			},
+		],
+		theme_color: "#171717",
+		background_color: "#e8ebf2",
+		display: "standalone",
+		scope: "/",
+		start_url: "/",
+		orientation: "portrait",
+	},
+};
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -13,12 +54,14 @@ export default defineConfig(({ mode }) => {
 		}
 	}
 	return {
+		base:"./",
 		plugins: [
 			react({
 				include: ["src/**/*.tsx", "src/**/*.ts", "src/**/*.scss"],
 			}),
 			tsconfigPaths(),
 			EnvironmentPlugin("all"),
+			VitePWA(manifestForPlugin),
 		],
 		define: {
 			"process.env": definedEnv,
