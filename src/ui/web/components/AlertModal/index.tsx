945d60ab9e66d2ac2dde69fba9782afbccb8469e
artur henrique <henrique-artur@hotmail.com>
2024-01-09 18:09:25 -0300

    Add alert model

---
+++ src/ui/web/components/AlertModal/index.tsx
@@ -0,0 +1,101 @@
+import {
+	useLastAlert,
+	useUpdateResponsibleAlert,
+} from "@web/contexts/panicButton/hooks";
+import { Col, Modal, Row, Typography } from "antd";
+import {
+	ForwardedRef,
+	forwardRef,
+	useCallback,
+	useImperativeHandle,
+	useState,
+} from "react";
+import styles from "./styles.module.scss";
+import Alert from "@models/Alert";
+import { useAccount } from "@web/contexts/auth/hooks";
+
+export interface AlertModalHandlers {
+	open(): void;
+}
+
+interface Props {
+	detailsOnClick(id: string): void;
+}
+
+function AlertModal(
+	{ detailsOnClick }: Props,
+	ref: ForwardedRef<AlertModalHandlers>
+) {
+	const [isOpen, setIsOpen] = useState<boolean>();
+	const alert = useLastAlert();
+	const getAlert = useUpdateResponsibleAlert();
+	const account = useAccount();
+
+	const open = useCallback(() => {
+		setIsOpen(true);
+	}, []);
+
+	useImperativeHandle(ref, () => ({ open }));
+
+	const close = useCallback(() => {
+		setIsOpen(false);
+	}, []);
+
+	const onOk = useCallback(async () => {
+		await getAlert(
+			Alert.fromForm({
+				id: alert?.id,
+				responsible: account?.id,
+				status: "ajuda_caminho",
+			})
+		);
+		detailsOnClick(alert?.id!);
+	}, [alert]);
+
+	const { Title, Paragraph } = Typography;
+
+	return (
+		<Modal
+			width={700}
+			title="Alerta Emitido"
+			open={isOpen}
+			onCancel={close}
+			okText="Pegar Alerta"
+			cancelText="Cancelar"
+			onOk={onOk}
+		>
+			<Row gutter={16}>
+				<Col span={6}>
+					<Title className={styles.title} level={4}>
+						Escola
+					</Title>
+					<Paragraph>{alert?.school.name}</Paragraph>
+				</Col>
+				<Col span={16}>
+					<Title className={styles.title} level={4}>
+						Endereço
+					</Title>
+					<Paragraph>
+						{alert?.school.address.completeAddres()}
+					</Paragraph>
+				</Col>
+			</Row>
+			<Row>
+				<Col span={6}>
+					<Title className={styles.title} level={4}>
+						Solicitante
+					</Title>
+					<Paragraph>{alert?.teacher.name}</Paragraph>
+				</Col>
+				<Col span={12}>
+					<Title className={styles.title} level={4}>
+						Telefone do Solicitante
+					</Title>
+					<Paragraph>{alert?.teacher.phone}</Paragraph>
+				</Col>
+			</Row>
+		</Modal>
+	);
+}
+
+export default forwardRef(AlertModal);
